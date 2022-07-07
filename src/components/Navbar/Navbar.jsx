dff94a6174354339163833bdac779a39abe82ff9
Matias Garrido <matias17garrido@gmail.com>
2022-07-07 19:33:01 -0300

    nav-desktop mui-installed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Navbar.css'
+import Logo from '../../assets/logo-nav.png'
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='container-nav'>
+      <div className='container-elements-nav'>
+        <div>
+          <img className='img-logo' src={Logo} alt="logo" />
+        </div>
+        <div className='container-titles-nav'>
+          <a href="/">PRODUCTOS</a>
+          <a href="/">SERVVICIO TÉCNCIO</a>
+          <a href="/">NOSOTROS</a>
+          <a href="/">CLIENTES</a>
+          <a href="/">CONTACTO</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
