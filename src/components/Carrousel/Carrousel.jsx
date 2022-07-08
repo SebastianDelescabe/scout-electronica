e117885279fbf77c7cb8db81bedbb65d7a1d0717
Seba <sebastiandelescabe@gmail.com>
2022-07-08 16:53:06 -0300

    style carrousel

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Home } from '../Home/Home';
+import './Carrousel.css'
 
-import { Navigation, Pagination } from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/bundle';
@@ -9,10 +10,16 @@ import 'swiper/css/bundle';
 export const Carrousel = () => {
     return (
         <Swiper
-            modules={[Navigation, Pagination]}
             slidesPerView={1}
+            loop={true}
+            autoplay={{
+                delay: 3500,
+                disableOnInteraction: false,
+            }}
             navigation
             pagination={{ clickable: true }}
+            className="swiper"
+            modules={[Navigation, Pagination, Autoplay]}
         >
             <SwiperSlide><Home /></SwiperSlide>
             <SwiperSlide><Home /></SwiperSlide>
