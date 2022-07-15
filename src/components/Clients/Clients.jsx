3f849cc75f5bb638bf523ca073a800575a622c13
Seba <sebastiandelescabe@gmail.com>
2022-07-14 22:49:01 -0300

    clients

--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -5,12 +5,15 @@ import './Clients.css';
 export const Clients = () => {
     return (
         <div className='clients' id="clients">
+            <div className='title-client'>
+                <p>CLIENTES</p>
+            </div>
             <div className='clients-banner'>
-                <span>20 años de experiencia en el mercado </span>
+                <span> Damos solución a los requerimientos de nuestros clientes</span>
             </div>
             <div className='clients-images'>
                 {clients && clients.map((image) => (
-                    <img src={image} alt="" />
+                    <img src={image} alt="clientsimage" />
                 ))}
             </div>
         </div>
