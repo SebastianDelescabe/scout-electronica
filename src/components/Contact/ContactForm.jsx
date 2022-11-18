a8df2d88586eb1fc73de3bcdd5fb37692c1d9da0
Seba <sebastiandelescabe@gmail.com>
2022-11-18 13:42:15 -0300

    final commit

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useFormik } from 'formik';
 import emailjs from "@emailjs/browser";
 import { ToastContainer, toast } from "react-toastify";
+import ReCAPTCHA from "react-google-recaptcha";
 import "react-toastify/dist/ReactToastify.css";
 import './Contact.css';
 
@@ -17,15 +18,27 @@ export const ContactForm = () => {
         request: '',
     }
 
+    const [captcha, setCaptcha] = useState(false)
+
+    function onChange() {
+        setCaptcha(true)
+    }
+
     const onSubmit = async (values, { resetForm }) => {
-        console.log(values);
-        emailjs.sendForm('service_h2zusuy', 'template_pqpj53j', form.current, 'x6-8LVIrjwT2UXfe-')
-            .then(() => {
-                toast.success("Email enviado correctamente");
-                resetForm({ company: "", email: "" })
-            }, (error) => {
-                toast.error("Error al enviar la consulta");
-            });
+        if (captcha) {
+            emailjs.sendForm('service_h2zusuy', 'template_pqpj53j', form.current, 'x6-8LVIrjwT2UXfe-')
+                .then(() => {
+                    toast.success("Email enviado correctamente");
+                    resetForm({ company: "", email: "" })
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 2000)
+                }, (error) => {
+                    toast.error("Error al enviar la consulta");
+                });
+        } else {
+            alert('completar captcha')
+        }
     }
 
     const formik = useFormik({ initialValues, onSubmit });
@@ -98,6 +111,11 @@ export const ContactForm = () => {
                             maxLength={1000}
                         />
                     </div>
+                    <ReCAPTCHA
+                        sitekey="6LdMERkjAAAAACb93r264r5WbOVDIZFHYCWCO4Ev"
+                        onChange={onChange}
+                        required
+                    />
                     <div className='form-button'>
                         <button type='submit' className="app__button" >
                             <span className="circle" aria-hidden="true">
