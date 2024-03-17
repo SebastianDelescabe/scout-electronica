9841367c4cf52a3b7063109c23e91c5125c147f6
Seba <seba@MacBook-Air-de-seba.local>
2024-03-17 17:32:06 -0300

    aos configure

--- src/components/Clients/ClientsBanner.jsx
+++ src/components/Clients/ClientsBanner.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React,{useEffect} from "react";
 import clientBanner1 from '../../assets/images/clientBanner1.png';
 import clientBanner2 from '../../assets/images/clientBanner2.png';
 import clientBanner3 from '../../assets/images/clientBanner3.png';
 import clientBanner4 from '../../assets/images/clientBanner4.png';
 import './Clients.css'
+import Aos from 'aos'
 
 export const ClientsBanner = () => {
+
+    useEffect(() => {
+        Aos.init({ duration: 1000 });
+    }, []);
+
     return (
         <div className='clients-banner'>
             <span>Diferenciales <strong> SCOUT</strong></span>
-            <div className='clients-banner-data'>
+            <div className='clients-banner-data' data-aos="fade-right">
                 <div>
                     <img src={clientBanner1} alt="" />
                     <p>Respuesta inmediata a consultas</p>
--- src/components/Clients/ClientsCards.jsx
+++ src/components/Clients/ClientsCards.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Compromiso from "../../assets/images/deal.png"
-import Watch from "../../assets/images/stopwatch.png"
-import Team from "../../assets/images/teamManagement.png"
-import { NavHashLink } from 'react-router-hash-link'
+import React,{useEffect} from "react";
+import Compromiso from "../../assets/images/deal.png";
+import Watch from "../../assets/images/stopwatch.png";
+import Team from "../../assets/images/teamManagement.png";
+import { NavHashLink } from "react-router-hash-link";
+import Aos from 'aos'
 
 export const ClientsCards = () => {
+  
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+}, []);
+
   return (
-    <>
-       <div className="container-nosotros">
-        <div className="container-elements-nosotros">
-            <div className="cotnainer-pq-serv-nosotros">
-              <div className="container-serv-nosotros">
+    <div className="container-nosotros">
+      <div className="container-elements-nosotros">
+        <div data-aos="fade-up" >
+          <div className="cotnainer-pq-serv-nosotros">
+            <div className="container-serv-nosotros">
               <NavHashLink to={"/conocenos#top"}>
                 <p className="title-serv-card">COMPROMISO</p>
                 <img className="img-serv-card" src={Compromiso} alt="deal" />
@@ -19,17 +25,18 @@ export const ClientsCards = () => {
                   de confiabilidad.
                 </p>
               </NavHashLink>
-              </div>
-              <div className="container-serv-nosotros">
+            </div>
+            <div className="container-serv-nosotros">
               <NavHashLink to={"/contact#top"}>
                 <p className="title-serv-card">TIEMPO</p>
                 <img className="img-serv-card" src={Watch} alt="watch" />
                 <p className="text-serv-card">
-                Plazos de entrega reales y confiables con costos competitivos en base al mercado.
+                  Plazos de entrega reales y confiables con costos competitivos
+                  en base al mercado.
                 </p>
-                </NavHashLink>
-              </div>
-              <div className="container-serv-nosotros">
+              </NavHashLink>
+            </div>
+            <div className="container-serv-nosotros">
               <NavHashLink to={"/service#top"}>
                 <p className="title-serv-card">SERVICIOS</p>
                 <img className="img-serv-card" src={Team} alt="team" />
@@ -37,11 +44,11 @@ export const ClientsCards = () => {
                   Servicios confiables en tiempo oportuno para garantizar la
                   satisfacción de nuestros clientes.
                 </p>
-                </NavHashLink>
-              </div>
+              </NavHashLink>
             </div>
+          </div>
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
